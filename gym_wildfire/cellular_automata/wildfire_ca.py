0f9c0987ef1ab2b7b5ff8bdcb7ad44e692cd1493
mlap <mlapeyro@berkeley.edu>
2020-11-06 12:20:50 -0800

    First working CA simulation

--- gym_wildfire/cellular_automata/wildfire_ca.py
+++ gym_wildfire/cellular_automata/wildfire_ca.py
@@ -6,21 +6,21 @@ from cellular_automaton import CellularAutomaton, MooreNeighborhood, CAWindow, E
 
 NO_FUEL = [0]
 UNBURNED_FUEL = [1]
-BURNING = [2]
+BURNING_FUEL = [2]
 BURNT = [3]
 
 class wildfireCA(CellularAutomaton):
     def __init__(self):
-        super().__init__(dimension=[200,200],
+        super().__init__(dimension=[20,20],
                          neighborhood=MooreNeighborhood(EdgeRule.IGNORE_MISSING_NEIGHBORS_OF_EDGE_CELLS))
 
     def init_cell_state(self, __):
         rand = random.random()
         if rand < 0.75:
             init = 1
-        if rand < 0.95 and rand >= 0.75:
+        if rand < 0.99 and rand >= 0.75:
             init = 0
-        if rand >= 0.95:
+        if rand >= 0.99:
             init = 2
         return [init]
 
@@ -31,16 +31,24 @@ class wildfireCA(CellularAutomaton):
         if last_cell_state == UNBURNED_FUEL and burning_neighbor_bool and rand < 0.58:
             new_cell_state = BURNING_FUEL
         if last_cell_state == BURNING_FUEL:
-            new_cell_state = BURNT
+            new_cell_state = BURNT 
         return new_cell_state
 
     @staticmethod
     def __is_neighbor_burning(neighbors):
-        if BURNING in neighbors:
+        if BURNING_FUEL in neighbors:
             return True
         else:
             return False
 
+def state_to_color(current_state):
+    if current_state == NO_FUEL: return 0
+    if current_state == UNBURNED_FUEL: return 200
+    if current_state == BURNING_FUEL: return 400
+    if current_state == BURNT: return 600
+
+
 if __name__=="__main__":
     CAWindow(cellular_automaton=wildfireCA(),
-             window_size=(1000, 830)).run(evolutions_per_second=5)
+             window_size=(1000, 830),
+             state_to_color_cb=state_to_color).run(evolutions_per_second=1)
