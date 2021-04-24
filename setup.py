13fd637ef29a34eeea7deba5084c29dad2e10b98
mlap <marcuslapeyrolerie@me.com>
2021-04-24 21:51:40 +0000

    Updating setup to install requisite packages

--- setup.py
+++ setup.py
@@ -4,5 +4,33 @@ from setuptools import setup
 setup(
     name="gym_wildfire",
     version="0.0.0",
-    packages=["gym_wildfire"],
+    packages=setuptools.find_packages(exclude=["docs", "scripts", "tests"]),
+    install_requires=["gym", "numpy", "pandas", "matplotlib"],
+    extras_require={
+        "tests": [
+            "stable-baselines3",
+            # Run tests and coverage
+            "pytest",
+            "pytest-cov",
+            "pytest-env",
+            "pytest-xdist",
+            # Type check
+            "pytype",
+            # Lint code
+            "flake8>=3.8",
+            # Sort imports
+            "isort>=5.0",
+            # Reformat
+            "black",
+        ],
+        "docs": [
+            "sphinx",
+            "sphinx-autobuild",
+            "sphinx-rtd-theme",
+            # For spelling
+            "sphinxcontrib.spelling",
+            # Type hints support
+            "sphinx-autodoc-typehints",
+        ],
+    },
 )
