76853c26d57db75d6d41fec9af0a5a4cae973035
mlap <marcuslapeyrolerie@me.com>
2021-04-27 20:48:56 +0000

    Took off make type

--- gym_wildfire/cellular_automata/alexandridis_et_al/__init__.py
+++ gym_wildfire/cellular_automata/alexandridis_et_al/__init__.py
@@ -1,5 +1,12 @@
 from gym_wildfire.cellular_automata.alexandridis_et_al.env_wildfire_ca import (
     EnvWildfireCA,
 )
-from gym_wildfire.cellular_automata.alexandridis_et_al.utils import *
-from gym_wildfire.cellular_automata.alexandridis_et_al.wildfire_ca import *
+from gym_wildfire.cellular_automata.alexandridis_et_al.utils import (
+    calc_pw,
+    get_slope,
+    get_wind,
+    tg,
+)
+from gym_wildfire.cellular_automata.alexandridis_et_al.wildfire_ca import (
+    wildfireCA,
+)
